aa6483fce9a46d4c09d0d21ad4d5a785c5c79a3f
Soumik Chaudhuri <soumik.chaudhuri2000@gmail.com>
2020-07-31 02:56:46 +0530

    styles added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import CurrencyGroup from './components/CurrencyGroup';
 function App() {
-	return <div className="App" />;
+	return (
+		<div className="currency">
+			<h1 className="lead">Currency Coverter</h1>
+			<CurrencyGroup />
+			<div className="equals">=</div>
+			<CurrencyGroup />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/CurrencyGroup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CurrencyGroup = () => {
+	return (
+		<div>
+			<input className="input" type="number" />
+			<select className="select">
+				<option>hey</option>
+				<option>bye</option>
+			</select>
+		</div>
+	);
+};
+
+export default CurrencyGroup;
