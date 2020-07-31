4c670b82a0d7077c5baafe28a2c0d6895fe6be50
Soumik Chaudhuri <soumik.chaudhuri2000@gmail.com>
2020-07-31 23:07:06 +0530

    fetched data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import CurrencyGroup from './components/CurrencyGroup';
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest';
 function App() {
+	const [ currencyOptions, setCurrencyOptions ] = useState([]);
+
+	const [ fromCurrency, setFromCurrency ] = useState();
+	const [ toCurrency, setToCurrency ] = useState();
+	useEffect(() => {
+		const fetchItems = async () => {
+			const result = await fetch(BASE_URL);
+
+			const data = await result.json();
+
+			const arr = [ data.base, ...Object.keys(data.rates) ];
+
+			setFromCurrency(data.base);
+			setToCurrency(Object.keys(data.rates)[0]);
+			setCurrencyOptions(arr);
+		};
+
+		fetchItems();
+	}, []);
 	return (
 		<div className="currency">
 			<h1 className="lead">Currency Coverter</h1>
-			<CurrencyGroup />
+			<CurrencyGroup
+				currencyOptions={currencyOptions}
+				selectedCurrency={fromCurrency}
+				onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+			/>
 			<div className="equals">=</div>
-			<CurrencyGroup />
+			<CurrencyGroup
+				currencyOptions={currencyOptions}
+				selectedCurrency={toCurrency}
+				onChangeCurrency={(e) => setToCurrency(e.target.value)}
+			/>
 		</div>
 	);
 }
--- src/components/CurrencyGroup.js
+++ src/components/CurrencyGroup.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CurrencyGroup = () => {
+const CurrencyGroup = ({ currencyOptions, selectedCurrency, onChangeCurrency }) => {
+	// const [ query, setText ] = useState('');
+	// const handleChange = (query) => {
+	// 	setText(query);
+	// 	getQuery(query);
+	// };
 	return (
 		<div>
 			<input className="input" type="number" />
-			<select className="select">
-				<option>hey</option>
-				<option>bye</option>
+			<select value={selectedCurrency} className="select" onChange={onChangeCurrency}>
+				{currencyOptions.map((option) => {
+					return (
+						<option key={option} value={option}>
+							{option}
+						</option>
+					);
+				})}
 			</select>
 		</div>
 	);
